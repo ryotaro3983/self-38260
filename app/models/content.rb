591b73ba9a5b89fbdcd50bfc2ddf78be5474ec9c
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-12-03 00:52:33 +0900

    レビュー編集/削除機能実装

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,7 +2,7 @@ class Content < ApplicationRecord
   validates :title, presence: true
   validates :genre, presence: true
   validates :memo, length: { maximum: 200 }
-  
+
   belongs_to :user
   has_many :reviews
   has_one_attached :image
