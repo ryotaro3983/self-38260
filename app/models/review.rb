9bf577a3fddf33e9ddf9bbdc666111bc78a841b1
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-12-12 00:55:48 +0900

    css手直し

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :comment, presence: true, length: { maximum: 200 }
+  validates :comment, presence: true, length: { maximum: 400 }
 
   belongs_to :user
   belongs_to :content
