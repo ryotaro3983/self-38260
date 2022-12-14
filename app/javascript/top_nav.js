be5362ad47d0af373339af7b376565579df94a06
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-12-14 13:03:48 +0900

    挙動修正

--- app/javascript/top_nav.js
+++ app/javascript/top_nav.js
@@ -1,7 +1,7 @@
 function fixedAnime(){
-  const headerH = $('#menu-nav').outerHeight(true);
+  // const headerH = $('#menu-nav').outerHeight(true);
   const scroll = $(window).scrollTop();
-  if(scroll>=headerH){
+  if(scroll>=185){
     $('#menu-nav').addClass('fixed');
   }else{
     $('#menu-nav').removeClass('fixed');
