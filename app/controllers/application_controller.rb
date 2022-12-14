40db6f47f47b6390cdf180d99df017073cc11aab
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-12-15 00:39:40 +0900

    アカウント情報編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,21 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'に誤りがあります、半角英数字を含めてください' },
+  validates :password, if: :password_required?, format: { with: VALID_PASSWORD_REGEX, message: 'に誤りがあります、半角英数字を含めてください' },
                        length: { maximum: 40 }
   has_many :posts
   has_many :contents
   has_many :reviews
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+    if params[:password].blank? && params[:password_confirmation].blank? 
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
